772a21f18a23a2f46c0d000e9a0d4cf20639357f
feor <5939320+FeorPrime@users.noreply.github.com>
2026-09-15 16:26:49 +0300

    variables modified

--- src/variables.cpp
+++ src/variables.cpp
@@ -6,4 +6,17 @@ int main (int, char **) {
     int variable_2 = 1234;
 
     int variable_3 {1234};
+
+    int a = 5;
+
+    double b = 5.5;
+    float f = 5.5f;
+
+    char c = (char)0xF8;
+
+    bool d = true;
+
+    const char *e = "Hello, World!";
+    return 0;
+
 }
